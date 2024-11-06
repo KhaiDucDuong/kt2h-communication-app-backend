a99917fabb4880ae8746ac6720d13130a8f82bb6
KhaiDucDuong <greenjohnny135@gmail.com>
2024-11-07 00:12:11 +0700

    authenticate and authorize stomp connections

--- src/main/java/hcmute/hhkt/messengerapp/configuration/WebSocketConfiguration.java
+++ src/main/java/hcmute/hhkt/messengerapp/configuration/WebSocketConfiguration.java
@@ -1,26 +1,117 @@
 package hcmute.hhkt.messengerapp.configuration;
 
+import hcmute.hhkt.messengerapp.util.JwtUtil;
+import hcmute.hhkt.messengerapp.util.SocketJwtAuthenticationToken;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
 import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationEventPublisher;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.authorization.SpringAuthorizationEventPublisher;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+import org.springframework.security.messaging.context.AuthenticationPrincipalArgumentResolver;
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.DefaultCsrfToken;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.web.socket.server.RequestUpgradeStrategy;
+import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.util.List;
+import java.util.Map;
 
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
+@EnableWebSocketSecurity
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private final WebSocketAuthenticator webSocketAuthenticator;
+
+    public WebSocketConfiguration(WebSocketAuthenticator webSocketAuthenticator) {
+        this.webSocketAuthenticator = webSocketAuthenticator;
+    }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
+    }
 
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+
+                    String bearerToken = accessor.getFirstNativeHeader(AUTHORIZATION_HEADER);
+                    final SocketJwtAuthenticationToken authenticationToken = webSocketAuthenticator.getAuthenticatedOrFail(bearerToken);
+
+                    accessor.setUser(authenticationToken);
+                }
+                return message;
+            }
+        });
+    }
+
+    @Bean
+    public ChannelInterceptor csrfChannelInterceptor(){
+        //disabling csrf
+        return new ChannelInterceptor() {
+
+        };
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/chatroom","/user");
+        registry.enableSimpleBroker("/chatroom","/user","/admin");
         registry.setUserDestinationPrefix("/user");
+    }
+
+    @Bean
+    AuthorizationManager<Message<?>> messageAuthorizationManager(
+            MessageMatcherDelegatingAuthorizationManager.Builder messages) {
+        messages.nullDestMatcher().authenticated();
+        messages.simpSubscribeDestMatchers("/user/**").hasAuthority("USER");
+        messages.simpMessageDestMatchers("/app/private-message").hasAuthority("USER");
+        messages.simpMessageDestMatchers("/app/group-chat").hasAuthority("USER");
+        messages.simpSubscribeDestMatchers("/user/admin/**").hasAuthority("ADMIN");
+        messages.simpDestMatchers("app/admin/**").hasAuthority("ADMIN");
+        messages.anyMessage().authenticated();
+
+        return messages.build();
     }
 }
